7cd46b95d1d43e3e3c8aa2da2038fd55698a99ad
ABskr <bskr26@gmx.de>
2021-04-15 16:44:33 +0200

    upload bereit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import listEndpoints from 'express-list-endpoints'
+import { join } from "path"
 import cors from 'cors'
 import studentRoutes from './students/index.js'
 import projectsRoutes from './projects/index.js'
+import filesRoutes from "./filesorganizer/index.js"
+import { getCurrentFolderPath } from "./lib/fs-tools.js"
 // import {
 //   badRequestErrorHandler,
 //   notFoundErrorHandler,
@@ -12,16 +15,19 @@ import projectsRoutes from './projects/index.js'
 
 const server = express()
 const port = process.env.PORT || 3001
+const pathToPublicFolder = join(getCurrentFolderPath(import.meta.url), "../public")
 
 const loggerMiddleware = (req, res, next) => {
   console.log(`Request method: ${req.method} ${req.url} -- ${new Date()}`)
   next()
 }
 
+server.use(express.static(pathToPublicFolder))
 server.use(cors())
 server.use(express.json())
 server.use('/students', loggerMiddleware, studentRoutes)
 server.use('/projects', projectsRoutes)
+server.use('/files', filesRoutes)
 
 // server.use(badRequestErrorHandler)
 // server.use(notFoundErrorHandler)
